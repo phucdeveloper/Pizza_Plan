8c43c5dd97c6ca26cdff9aa209b9d72db8bd4dd1
Philip <44945136+phucdeveloper@users.noreply.github.com>
2020-08-01 10:31:41 +0700

    Update project 10:31 PM 1/08/2020.

--- app/src/main/java/com/philipstudio/pizzaplan/view/MapsActivity.java
+++ app/src/main/java/com/philipstudio/pizzaplan/view/MapsActivity.java
@@ -1,16 +1,11 @@
 package com.philipstudio.pizzaplan.view;
 
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
-
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,15 +39,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
 
         btnChon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (TextUtils.isEmpty(result)){
+                if (TextUtils.isEmpty(result)) {
                     Toast.makeText(MapsActivity.this, "Bạn chưa chọn địa điểm", Toast.LENGTH_SHORT).show();
-                }
-                else{
+                } else {
                     Intent intent = new Intent();
                     intent.putExtra("diachi", result);
                     setResult(RESULT_OK, intent);
@@ -69,45 +65,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setZoomGesturesEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        mMap.setOnMapClickListener(this);
 
-        LatLng latLng = new LatLng(10.803283, 106.702992);
-        showMarkerInGoogleMap(latLng, "Current Marker");
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
-        Geocoder geocoder = new Geocoder(this, Locale.ENGLISH);
-        try {
-            List<Address> listDiaChi = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
-            if (listDiaChi != null && listDiaChi.size() > 0 ){
-                Address address = listDiaChi.get(0);
-                result = address.getAddressLine(0) + ", " + address.getLocality();
-                latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                showMarkerInGoogleMap(latLng, result);
-                edtTimKiem.setText(result);
-            }
-
-        } catch (IOException e) {
-            Log.e("TAG", "Impossible to connect to Geocoder", e);
-        }
+        showMarkerInGoogleMap(latLng, "Current Marker");
     }
 
-    private void showMarkerInGoogleMap(LatLng latLng, String title) {
+    private void showMarkerInGoogleMap(LatLng latLng, String text) {
         mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(latLng).title(title));
+        mMap.addMarker(new MarkerOptions().position(latLng).title(text));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
+
 }
